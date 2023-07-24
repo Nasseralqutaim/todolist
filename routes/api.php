37bed25c2787d7af9dd2e5999f7dc60837693091
Nasser Alqutaim <n.alqutaim@gmail.com>
2023-07-23 17:21:15 -0700

    debug update function and test using Postman

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/items', [ItemController::class, 'index']);
 Route::prefix('/item')->group(function () {
     Route::post('/store', [ItemController::class, 'store']);
-    Route::put('/store', [ItemController::class, 'update']);
+    Route::put('/{id}', [ItemController::class, 'update']);
     Route::delete('/store', [ItemController::class, 'destroy']);
 });
